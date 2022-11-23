5e776906c6960f03c8f27972ce0e32c85fe74819
dev-suleymanov <dev.suleymanov@gmail.com>
2022-11-23 22:57:15 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -4,17 +4,33 @@ import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 
 public class Point {
-
     private int x;
-
     private int y;
+    private int z;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
+    public static void main(String[] args) {
+        Point first = new Point(1, 1);
+        Point second = new Point(2, 5);
+        double result = first.distance(second);
+        System.out.println(result);
     }
 }
---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.oop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+
+public class PointTest {
+    @Test
+    public void when11And25When4Dot123() {
+        Point first = new Point(1,1);
+        Point second = new Point(2,5);
+        double result = first.distance(second);
+        double expected = 4.123;
+        assertThat(result).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void when25And61When5Dot657() {
+        Point first = new Point(2,5);
+        Point second = new Point(6,1);
+        double result = first.distance(second);
+        double expected = 5.657;
+        assertThat(result).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void when111And253When4Dot583() {
+        Point first = new Point(1,1, 1);
+        Point second = new Point(2,5, 3);
+        double result = first.distance3d(second);
+        double expected = 4.583;
+        assertThat(result).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void when329And468When4Dot243() {
+        Point first = new Point(3,2, 9);
+        Point second = new Point(4,6, 8);
+        double result = first.distance3d(second);
+        double expected = 4.243;
+        assertThat(result).isCloseTo(expected, offset(0.001));
+    }
+}
