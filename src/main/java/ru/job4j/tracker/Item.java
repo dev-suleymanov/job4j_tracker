fb31b6ce23b077c07cacf39b4fcef1230bf2146a
dev-suleymanov <dev.suleymanov@gmail.com>
2022-12-02 13:43:45 +0300

    8. Date. Отображение даты. [#310017 #366763]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,10 +7,6 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
     public Item() {
 
     }
@@ -22,6 +18,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
