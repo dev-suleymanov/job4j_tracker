8adab5d7f9a50e209863fde280805c9d932f993c
dev-suleymanov <dev.suleymanov@gmail.com>
2022-12-07 01:24:29 +0300

    5. Tracker - хранилище [#396 #368255]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -34,7 +34,7 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        Item[] result = new Item[items.length];
+        Item[] result = new Item[size];
         int length = 0;
         for (int i = 0; i < size; i++) {
             if (items[i].getName().equals(key)) {
