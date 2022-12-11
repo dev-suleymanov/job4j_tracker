1b7b6b0efb68506ec0ba07f198aa59949c140b2a
dev-suleymanov <dev.suleymanov@gmail.com>
2022-12-11 18:19:34 +0300

    3.5. Реализация класса StartUI. Вывод заявки по id [#500748]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -46,6 +46,16 @@ public class StartUI {
                 } else {
                     System.out.println("Error by delete application");
                 }
+            } else if (select == 4) {
+                System.out.println("=== Find item by id ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Error by find application by id");
+                }
             } else if (select == 6) {
                 run = false;
             }
