9aee78322cd64665f1583c2a309005707e0f8195
dev-suleymanov <dev.suleymanov@gmail.com>
2022-12-14 14:53:34 +0300

    9. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -42,5 +42,6 @@ public class StartUITest {
         StartUI.editItem(new StubInput(answers), tracker);
         Item edited = tracker.findById(item.getId());
         assertThat(edited.getName()).isEqualTo("null");
+
     }
 }
