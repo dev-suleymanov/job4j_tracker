51cb87b828453060ec5abc50c1505b027f46bbbe
dev-suleymanov <dev.suleymanov@gmail.com>
2022-12-16 18:06:17 +0300

    12.0. Написать тесты на StartUI. [#785]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -28,7 +28,7 @@ public class StartUI {
                 new DeleteAction(),
                 new FindByIdAction(),
                 new FindByNameAction(),
-                new ExitAction()
+                new Exit()
         };
         new StartUI().init(input, tracker, actions);
     }
