f02a73288a7fef305aa0739e07b871e2f06b9e95
dev-suleymanov <dev.suleymanov@gmail.com>
2023-01-21 17:37:31 +0300

    1. Уникальные задачи. [#212653]

---
+++ src/test/java/ru/job4j/collection/FullSearchTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FullSearchTest {
+    @Test
+    public void extractNumber() {
+        List<Task> tasks = Arrays.asList(
+                new Task("1", "First desc"),
+                new Task("2", "Second desc"),
+                new Task("1", "First desc")
+        );
+        assertThat(new FullSearch().extractNumber(tasks)).containsAll(Arrays.asList("1", "2"));
+    }
+}
