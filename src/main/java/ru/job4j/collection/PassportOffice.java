d45f88307951c5f016a9876cdc8d62426ff362c3
dev-suleymanov <dev.suleymanov@gmail.com>
2023-01-24 18:44:14 +0300

    1. Паспорт и жители [#212656 #384214]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -7,27 +7,15 @@ public class PassportOffice {
     private Map<String, Citizen> citizens = new HashMap<>();
 
     public boolean add(Citizen citizen) {
-        boolean result = true;
-        for (String key : citizens.keySet()) {
-            if (citizens.get(key).getPassport().equals(citizen.getPassport())) {
-                result = false;
-                break;
-            }
-        }
-        if (result) {
+        boolean result = false;
+        if (!citizens.containsKey(citizen.getPassport())) {
             citizens.put(citizen.getPassport(), citizen);
+            result = true;
         }
         return result;
     }
 
     public Citizen get(String passport) {
-        Citizen result = null;
-        for (String key : citizens.keySet()) {
-            if (citizens.get(key).getPassport().equals(passport)) {
-                result = citizens.get(passport);
-                break;
-            }
-        }
-        return result;
+        return citizens.get(passport);
     }
 }
