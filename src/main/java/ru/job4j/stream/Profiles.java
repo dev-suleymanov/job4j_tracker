c7fb7fc464b6e0e830de445f5b0e854be67cbfaf
dev-suleymanov <dev.suleymanov@gmail.com>
2023-02-17 11:33:41 +0300

    6. Список адресов [#110062 #392401]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(a -> a.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 
