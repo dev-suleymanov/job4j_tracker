b4b96fe8a8b46ce449c1dc4fc78f83312e063d26
dev-suleymanov <dev.suleymanov@gmail.com>
2023-09-20 23:20:58 +0300

    2. Трекер SQL [#1734 #446303]

--- src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -1,29 +1,23 @@
 package ru.job4j.tracker;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.Assert;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class ItemAscByNameTest {
+
     @Test
-    public void whenSortingNameByAscending() {
-        List<Item> items = new ArrayList<>();
-        items.add(new Item("Rustam"));
-        items.add(new Item("Vadim"));
-        items.add(new Item("Alan"));
-        items.add(new Item("Rauf"));
-        items.add(new Item("Oleg"));
-        items.sort(new ItemAscByName());
-        List<Item> expected = List.of(
-                new Item("Alan"),
-                new Item("Oleg"),
-                new Item("Rauf"),
-                new Item("Rustam"),
-                new Item("Vadim")
-        );
-        assertThat(items).isEqualTo(expected);
+    public void compare() {
+        Item item1 = new Item(1, "Sasha");
+        Item item2 = new Item(2, "Dasha");
+        Item item3 = new Item(3, "Aleksey");
+        Item item4 = new Item(4, "Jora");
+        List<Item> items = Arrays.asList(item1, item2, item3, item4);
+        Collections.sort(items, new ItemAscByName());
+        List<Item> expected = Arrays.asList(item3, item2, item4, item1);
+        Assert.assertArrayEquals(new List[]{expected}, new List[]{items});
     }
 }
--- src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -1,29 +1,23 @@
 package ru.job4j.tracker;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.Assert;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class ItemDescByNameTest {
+
     @Test
-    public void whenSortingNameByDecreasing() {
-        List<Item> items = new ArrayList<>();
-        items.add(new Item("Rustam"));
-        items.add(new Item("Vadim"));
-        items.add(new Item("Alan"));
-        items.add(new Item("Rauf"));
-        items.add(new Item("Oleg"));
-        items.sort(new ItemDescByName());
-        List<Item> expected = List.of(
-                new Item("Vadim"),
-                new Item("Rustam"),
-                new Item("Rauf"),
-                new Item("Oleg"),
-                new Item("Alan")
-        );
-        assertThat(items).isEqualTo(expected);
+    public void compare() {
+        Item item1 = new Item(1, "Sasha");
+        Item item2 = new Item(2, "Dasha");
+        Item item3 = new Item(3, "Aleksey");
+        Item item4 = new Item(4, "Jora");
+        List<Item> items = Arrays.asList(item1, item2, item3, item4);
+        Collections.sort(items, new ItemDescByName());
+        List<Item> expected = Arrays.asList(item1, item4, item2, item3);
+        Assert.assertArrayEquals(new List[]{expected}, new List[]{items});
     }
 }
