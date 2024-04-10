ecf6ae46fdfcfdee3d2a37ce69f18897addfb62a
dev-suleymanov <dev.suleymanov@gmail.com>
2024-04-10 13:50:54 +0300

    2. @Setter, @Getter и другие [#504889 #490698]

--- src/main/java/ru/job4j/lombok/CategoryUsage.java
+++ src/main/java/ru/job4j/lombok/CategoryUsage.java
@@ -2,9 +2,9 @@ package ru.job4j.lombok;
 
 public class CategoryUsage {
     public static void main(String[] args) {
-        Category category1 = new Category(1);
+        Category category1 = new Category(1L);
         category1.setName("first");
-        Category category2 = new Category(1);
+        Category category2 = new Category(1L);
         category2.setName("second");
         System.out.println(category1.equals(category2));
     }
